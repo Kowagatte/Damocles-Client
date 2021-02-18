1b576878d455bfeb8243baf4e632e6633daecfbc
Nick Ryan <nnryanp@gmail.com>
2021-02-17 20:40:59 -0700

    Initial Unity Project move to Github

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    
+    public CharacterController controller;
+    
+    private float movementSpeed = 6f;
+    private float velocity = 0f;
+    private float gravity = -9.811f;
+    private float jumpHeight = 1f;
+    private bool isOnGround = false;
+
+    // Update is called once per frame
+    private void Update() {
+        isOnGround = controller.isGrounded;
+        if (isOnGround && velocity < 0) {
+            velocity = 0f;
+        }
+        
+        var movement = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+        controller.Move(movement * movementSpeed * Time.deltaTime);
+
+        if (isOnGround && Input.GetKeyDown(KeyCode.Space)) {
+            velocity += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+        
+        velocity += gravity * Time.deltaTime;
+        controller.Move(new Vector3(0,velocity * Time.deltaTime,0));
+
+    }
+}
